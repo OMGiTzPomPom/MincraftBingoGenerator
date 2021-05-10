65106f3d5c1541ea4911bea9ce3454e68bc75d16
JunkJumper <pauline.srifi@gmail.com>
2021-05-10 18:08:46 +0200

    start

---
+++ src/Launch.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+
+public class Launch {
+
+	public List<String> l = new ArrayList<>();
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
