933ad1726a699fc1e98b5fc58a1a5c726f9ab886
JunkJumper <pauline.srifi@gmail.com>
2021-05-14 17:33:37 +0200

    revoke public static main args[] to just a sysout

--- src/fr/junkjumper/bingogo/commands/runa.java
+++ src/fr/junkjumper/bingogo/commands/runa.java
@@ -1,18 +1,7 @@
 package fr.junkjumper.bingogo.commands;
-
 import fr.junkjumper.bingogo.Bingo;
-
 public class runa {
-	
-
 	public static void main(String[] args) {
-		
-		Bingo b = new Bingo();
-		b.generateBingo(9);
-		
 		System.out.println("main start testing for sysout");
-
-		
 	}
-
 }
